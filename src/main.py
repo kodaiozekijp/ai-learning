d9dfa3cdd39c1cf6ced6d706cbefb0fddec17cd0
kodaiozekijp <kodai.ozeki@gmail.com>
2026-05-02 13:53:17 +0900

    ChatCompletionsに合わせた書き方に更新

--- src/main.py
+++ src/main.py
@@ -19,6 +19,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 def get_colors(message: str) -> list[str]:
+    messages = [
+        {"role": "system", "content": "You are a palette generating assistant that responds to text prompts for color palettes. You should generate color palettes that fit the theme, mood, or instructions in the prompt. The palettes should be between 2 and 8 colors."},
+        {"role": "user", "content": "Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea."},
+        {"role": "assistant", "content": '["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]'},
+        {"role": "user", "content": "Convert the following verbal description of a color palette into a list of colors: sage, nature, earth"},
+        {"role": "assistant", "content": '["#EDF1D6", "#9DC08B", "#609966", "#40513B"]'},
+        {"role": "user", "content": f"Convert the following verbal description of a color palette into a list of colors: {message}"},
+    ]
+
     prompt = f"""
     You are a palette generating assistant that responds to text prompts for color palettes.
     You should generate color palettes that fit the theme, mood, or instructions in the prompt.
@@ -38,7 +47,8 @@ def get_colors(message: str) -> list[str]:
 
     response = client.chat.completions.create(
         model="gpt-4.1-mini",
-        messages=[{"role": "system", "content": prompt}],
+        # messages=[{"role": "system", "content": prompt}],
+        messages=messages,
         max_tokens=100
     )
     return json.loads(response.choices[0].message.content)
